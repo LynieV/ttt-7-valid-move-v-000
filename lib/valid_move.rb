80af0e9e95afe4e5a230e0e69325be53afb1d595
Valerie McClimon <vmcclimon33@yahoo.com>
2017-11-15 20:12:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,6 +12,6 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
   else board[index] != "X" && board[index] != "O"
-    false
+    true
   end
 end
